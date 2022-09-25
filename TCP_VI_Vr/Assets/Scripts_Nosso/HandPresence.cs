7d26a2c80f3b4c011b7f8a9fdb45907fc86ccf80
pigseverywhere <62438027+pigseverywhere@users.noreply.github.com>
2022-09-25 15:28:13 -0300

    Retirada Controles, Ajuste das duas Mãos, Mao controle Inplementada

---
+++ TCP_VI_Vr/Assets/Scripts_Nosso/HandPresence.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class HandPresence : MonoBehaviour
+{   
+    int framesToSkip;
+
+
+    public InputDeviceCharacteristics ControllerCharacteristics;
+
+    private InputDevice targetDevice;
+
+  
+    public GameObject handModelPrefab;
+
+    private GameObject spawnedHandModel;
+     //Start is called before the first frame update
+    void Start()
+    {
+        
+        framesToSkip = 10;
+        
+    }
+
+ 
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(framesToSkip >= 2){
+            framesToSkip--;
+
+        }
+         if(framesToSkip == 1){
+
+
+            List<InputDevice> devices = new List<InputDevice>();
+        InputDevices.GetDevicesWithCharacteristics(ControllerCharacteristics, devices);
+
+       // InputDevices.GetDevices(devices);
+
+
+        foreach(var item in devices)
+        {
+
+            Debug.Log(item.name + item.characteristics);
+
+
+
+        }
+        if(devices.Count > 0){
+
+            targetDevice = devices[0];
+            
+            
+            spawnedHandModel = Instantiate(handModelPrefab,transform);
+        }
+
+        framesToSkip = 0;
+        }
+
+
+
+
+
+
+        //targetDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool primaryButtonValue);
+       // if(primaryButtonValue){
+
+       //           Debug.Log("pressing Primary Button");
+       // }
+           
+
+       // targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue);
+        //if(triggerValue > 0.1f){
+
+         //   Debug.Log("Trigger pressed" + triggerValue);
+        //}
+
+
+       // targetDevice.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 primary2DAxisValue);
+        //if(primary2DAxisValue != Vector2.zero){
+
+         //   Debug.Log("Primary Touchpad" + primary2DAxisValue);
+        //}
+
+    }
+}
