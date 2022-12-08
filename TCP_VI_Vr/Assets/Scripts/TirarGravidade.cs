bdb3771002195f69055a605e9604c3078cbdaadd
bruno-reload <bruno-reload@live.com>
2022-12-07 21:23:30 -0300

    cupdate tiraGravidade

--- TCP_VI_Vr/Assets/Scripts/TirarGravidade.cs
+++ TCP_VI_Vr/Assets/Scripts/TirarGravidade.cs
@@ -4,55 +4,38 @@ using UnityEngine;
 
 public class TirarGravidade : MonoBehaviour
 {
-   
-
-   private GameObject target;
+    private GameObject target;
     Rigidbody item;
-    
 
 
-   
+    void OnTriggerEnter(Collider other)
+    {
 
-    void OnTriggerEnter(Collider other) {
-
-          if(other.tag == "roupa"){
-
-            Debug.Log ("entrou");
+        if (other.tag == "roupa")
+        {
+            Debug.Log("entrou");
             item = other.GetComponent<Rigidbody>();
             item.useGravity = false;
             item.constraints = RigidbodyConstraints.FreezePosition;
-           // item.isKinematic = true;
-
-
-          }
-          
-           
-
-       }
-
-         void OnTriggerExit(Collider other) {
-
-
-          if(other.tag == "roupa"){
-            
-          Debug.Log ("saiu");
-          item = other.GetComponent<Rigidbody>();
-          Ativar();
-
-          }
-           
-       
-            
+            // item.isKinematic = true;
         }
+    }
 
-        public void Ativar(){
-
-          item.constraints = RigidbodyConstraints.None;
-          item.useGravity = true;
-          
-          //item.isKinematic = false;
-
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "roupa")
+        {
+            Debug.Log("saiu");
+            item = other.GetComponent<Rigidbody>();
+            Ativar();
         }
+    }
 
+    public void Ativar()
+    {
+        item.constraints = RigidbodyConstraints.None;
+        //item.useGravity = true;
 
+        //item.isKinematic = false;
+    }
 }
